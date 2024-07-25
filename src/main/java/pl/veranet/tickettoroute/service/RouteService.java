94921f03862de684e067a6715a21ff4bda2cf1d3
veranet <halatsiankova@gmail.com>
2024-07-25 17:45:43 +0200

    add the ability to load routes from json file

--- src/main/java/pl/veranet/tickettoroute/service/RouteService.java
+++ src/main/java/pl/veranet/tickettoroute/service/RouteService.java
@@ -1,9 +1,13 @@
 package pl.veranet.tickettoroute.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jgrapht.Graph;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import pl.veranet.tickettoroute.dto.Price;
 import pl.veranet.tickettoroute.enams.Currency;
@@ -12,6 +16,8 @@ import pl.veranet.tickettoroute.repository.RouteRepository;
 
 import org.springframework.beans.factory.annotation.Value;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -41,6 +47,21 @@ public class RouteService {
         int numberOfSegments = Double.valueOf(Math.ceil(dijkstraShortestPath.getPath(from, to).getWeight())).intValue();
         double optimalPrice = calculatePrice(numberOfSegments);
         return new Price(numberOfSegments, BigDecimal.valueOf(optimalPrice), Currency.GBP);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadRoutes() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<Route>> typeReference = new TypeReference<>(){};
+        InputStream inputStream = getClass().getResourceAsStream("/routes.json");
+
+        List<Route> routes;
+        try {
+            routes = objectMapper.readValue(inputStream, typeReference);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load routes from JSON.");
+        }
+        routeRepository.saveAll(routes);
     }
 
     private List<Route> getAllRoutes(String from, String to) {
